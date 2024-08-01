45b212ec88ce369112f3147b9b36499fa574d33c
Mansen <husenalasady21@gmail.com>
2024-08-01 16:57:15 +0300

    Add identity matrix

--- math.cpp
+++ math.cpp
@@ -14,10 +14,20 @@ namespace math
     concept multipliable = lhs_cols == rhs_rows;
 
     template <typename T, int n_rows, int n_cols>
+    requires (n_rows > 0 && n_cols > 0)
     class matrix
     {
         T data[n_rows][n_cols];
         public:
+
+        const static inline matrix<T, n_rows, n_cols> I = matrix::map([](int row, int col)
+        {
+            static_assert(n_rows == n_cols);
+            if (row == col)
+                return T{1};
+            else
+                return T{0};
+        });
 
         //returns pointer to row (use this when mutation is desired)
         T* operator[](int idx) {return this->data[idx];}
@@ -25,12 +35,21 @@ namespace math
         T operator()(size_t r, size_t c)const{return data[r][c];}
 
         //returns mapped copy of object
-        matrix map(std::function<T(T value, int row, int col)>mapping) const
+        matrix map(std::function<T(T value, int row, int col)>mapping)
         {
             matrix<T, n_rows, n_cols> result;
             for(size_t i = 0; i < n_rows; ++i)
                 for(size_t j = 0; j < n_cols; ++j)
                     result[i][j] = mapping((*this)(i, j), i, j);
+            return result;
+        }
+        //maps an empty matrix to return value of mapping()
+        static matrix map(std::function<T(int row, int col)>mapping)
+        {
+            matrix<T, n_rows, n_cols> result;
+            for(size_t i = 0; i < n_rows; ++i)
+                for(size_t j = 0; j < n_cols; ++j)
+                    result[i][j] = mapping(i, j);
             return result;
         }
         //performs action on each element of matrix (allows mutation!)
@@ -40,11 +59,17 @@ namespace math
                 for(size_t j = 0; j < n_cols; ++j)
                     action((*this)[i][j], i, j);
         }
+        void for_each_const(std::function<void(T value, int row, int col)>action) const
+        {
+            for(size_t i = 0; i < n_rows; ++i)
+                for(size_t j = 0; j < n_cols; ++j)
+                    action((*this)(i, j), i, j);
+        }
 
-        void print(std::ostream& stream)
+        void print(std::ostream& stream) const
         {
             stream << "{";
-            (*this).for_each([&](T value, int row, int col)
+            (*this).for_each_const([&](T value, int row, int col)
             {
                 if (col == 0)
                     stream << "{";
@@ -89,6 +114,11 @@ namespace math
                 return sum;
             });
         }
+        matrix operator*(const float coeff) const
+        {
+            return map([&](T value, int row, int col){return coeff*value;});
+        }
+
     };
 };
 
@@ -101,10 +131,11 @@ int main([[maybe_unused]] int argc, [[maybe_unused]] char** argv)
     matrix<int, 2, 2> m2;
     m2.for_each([](int& value, int row, int col){value = col;});
 
-    m1.print(std::cout);
-    m2.print(std::cout);
+ //   m1.print(std::cout);
+ //   m2.print(std::cout);
 
-    (m1*m2).print(std::cout);
+    matrix<int, 3, 3>::I.print(std::cout);
+    //(m1*m2).print(std::cout);
 
     return 0;   
 }
